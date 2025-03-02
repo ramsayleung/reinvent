7c7872d19b76b0e571711cbdf43b16c731774e51
Ramsay Leung <ramsayleung@gmail.com>
2025-03-02 11:41:33 -0800

    Implement copyFile based on streaming IO.

---
+++ file_backup/stream-copy.ts
@@ -0,0 +1,37 @@
+/*
+Streaming I/O
+
+Write a small program using fs.createReadStream and
+fs.createWriteStream that copies a file piece-by-piece
+instead of reading it into memory and then writing it out again.
+ */
+
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const streamCopyFile = (source: string, destination: string) => {
+  const readStream = fs.createReadStream(source);
+  const writeStream = fs.createWriteStream(destination);
+
+  readStream.on('error', (err) => {
+    console.error(`Error reading file: ${err.message}`);
+  });
+
+  writeStream.on('error', (err) => {
+    console.error(`Error writing file: ${err.message}`);
+  });
+
+  writeStream.on('finish', () => {
+    console.log(`File copied from ${source} to ${destination}`);
+  });
+
+  readStream.pipe(writeStream);
+}
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const source = path.join(__dirname, 'stream-copy.ts');
+const destination = '/tmp/stream-copy.ts';
+streamCopyFile(source, destination);
