33b1e1f1d879d90f11d46c6d6a4d53ab575ce995
Ramsay Leung <ramsayleung@gmail.com>
2025-04-28 22:15:09 -0700

    Adjust the interface for Render.

--- layout_engine/render.ts
+++ layout_engine/render.ts
@@ -1,7 +1,7 @@
 import { Placeable } from "./easy_mode";
 import { PlacedBlock, PlacedCol, PlacedRow } from "./placed";
 
-export const render = (root: Node) => {
+export const render = (root: Renderable) => {
   root.place(0, 0);
   const width = root.getWidth();
   const height = root.getHeight();
@@ -10,14 +10,11 @@ export const render = (root: Node) => {
   return screen.map(row => row.join('')).join('\n');
 }
 
-export interface Renderable {
-  render(screen: string[][], fill: string): void;
-}
-
-export interface Node extends Renderable, Placeable {
+export interface Renderable extends Placeable {
   x0: number;
   y0: number;
-  children?: Node[];
+  children?: Renderable[];
+  render(screen: string[][], fill: string): void;
 }
 
 const makeScreen = (width: number, height: number): string[][] => {
@@ -28,7 +25,7 @@ const makeScreen = (width: number, height: number): string[][] => {
   return screen;
 }
 
-const draw = (screen: string[][], node: Node, fill: string = null): string => {
+const draw = (screen: string[][], node: Renderable, fill: string = null): string => {
   fill = nextFill(fill);
   node.render(screen, fill);
   node.children?.forEach(child => {
@@ -41,24 +38,24 @@ const nextFill = (fill: string): string => {
   return (fill === null) ? 'a' : String.fromCharCode(fill.charCodeAt(0) + 1)
 }
 
-const drawBlock = (screen: string[][], node: Node, fill: string) => {
+const drawBlock = (screen: string[][], node: Renderable, fill: string) => {
   for (let ix = 0; ix < node.getWidth(); ix += 1) {
     for (let iy = 0; iy < node.getHeight(); iy += 1) {
       screen[node.y0 + iy][node.x0 + ix] = fill;
     }
   }
 }
-export class RenderedBlock extends PlacedBlock implements Node {
+export class RenderedBlock extends PlacedBlock implements Renderable {
   render(screen: string[][], fill: string) {
     drawBlock(screen, this, fill);
   }
 }
 
-export class RenderedCol extends PlacedCol implements Node {
+export class RenderedCol extends PlacedCol implements Renderable {
   // We need to override the children property to make it compatible with Node
-  declare children: Node[];
+  declare children: Renderable[];
 
-  constructor(...children: Node[]) {
+  constructor(...children: Renderable[]) {
     super(...children);
   }
   render(screen: string[][], fill: string): void {
@@ -66,11 +63,11 @@ export class RenderedCol extends PlacedCol implements Node {
   }
 }
 
-export class RenderedRow extends PlacedRow implements Node {
+export class RenderedRow extends PlacedRow implements Renderable {
   // We need to override the children property to make it compatible with Node
-  declare children: Node[];
+  declare children: Renderable[];
 
-  constructor(...children: Node[]) {
+  constructor(...children: Renderable[]) {
     super(...children);
   }
   render(screen: string[][], fill: string): void {
